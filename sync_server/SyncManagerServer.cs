e8f94ad9de52968111643dce991778349a7a9212
DrewNF <hause.blackwarhol@gmail.com>
2015-06-03 23:23:24 +0200

    Final Modify
    Create a better managment with the third thread that serve each client
    connection, write the function that converts from / to JSON string and
    String[] usefull to use the StreamReader and Writer to write command

--- sync_server/SyncManagerServer.cs
+++ sync_server/SyncManagerServer.cs
@@ -27,6 +27,7 @@ namespace sync_server
 		public delegate void StatusDelegate(String s, int type);
 		private StatusDelegate statusDelegate;
 		private Thread listenThread;
+        private Thread operationThread;
 		private String workDir;
         private int version ;
 		private bool serverStopped = false;
@@ -75,160 +76,50 @@ namespace sync_server
 
 		private void listenForConnections()
 		{
+
+
+
+
 			// thread that waiting for connections
 			while (!serverStopped)
 			{
+                //Get Connection
 				statusDelegate("Waiting for connections", fSyncServer.LOG_INFO);
 				TcpClient client = tcpServer.AcceptTcpClient();
 				statusDelegate("Client connected", fSyncServer.LOG_INFO);
-				NetworkStream networkStream = client.GetStream();
-				//byte[] data = new Byte[256];
-				
-                //CREAZIONE NUOVO THREAD SERVIZIO CLIENT
-                String message;
-				int i;
-				while(client.Connected){
-					//i = networkStream.Read(data, 0, data.Length);
-					//message = System.Text.Encoding.ASCII.GetString(data, 0, i);
-					//statusDelegate(message, fSyncServer.LOG_NORMAL);
-                    message=this.getClientCommand();
-				}
-			}
+
+
+                //Check User Credential
+
+
+                //Instantiate Client Slave
+                Thread clientThread = new Thread(new ParameterizedThreadStart(doSync));
+			    clientThread.IsBackground = true;
+                clientThread.Start(client);
+            }
 		}
-        //Genera il file checksum delle cartelle e ricorsivamente dei file interni
 
-        private void generateServerChecksum(String dir)
+
+        //Client Slave Main Operation
+        private void doSync(object client)
+		{
+
+                TcpClient cl = (TcpClient) client;
+                NetworkStream netStream = cl.GetStream();
+                StreamReader sr = new StreamReader(netStream);
+                StreamWriter sw = new StreamWriter(netStream);
+                String cmd =sr.ReadLine();
+                String[] command = this.getCommand(cmd);
+				
+		}
+
+        private void doCommand(String[] stringCMD)
         {
-            String pattern= "["+ "_" + version.ToString() + "\\" + "Z" + "]" ;
-            string[] fileList = Directory.GetFiles(dir);
-            foreach (string filePath in fileList)
-            {
-                if(Regex.IsMatch(filePath, pattern))
-                {
-                    serverFileChecksum.Add(new FileChecksum(filePath));
-                }
-            }
 
-            // Recurse into subdirectories of this directory.
-            string[] subdirectoryList = Directory.GetDirectories(dir);
-            foreach (string subdirectoryPath in subdirectoryList)
-            {
-                this.generateServerChecksum(subdirectoryPath);
-            }
         }
+		/*
 
 		
-
-
-
-
-		private void doSync()
-		{
-            //Recieve Client Dir
-            String clientDir = getClientDir();
-            //Check if Client Dir is Equal to Work Dir esle Esare all and Start New Sync Session
-            if(clientDir!=workDir)
-            {
-                Directory.Delete(workDir, true);
-                workDir = clientDir;
-                Directory.CreateDirectory(workDir);
-
-                //CANCEL TO DB MUST BE ADDED
-            }
-
-			
-			this.sendCheckList(serverFileChecksum);
-
-			
-
-			// confronta checksum
-			int pos;
-			foreach(FileChecksum file in serverFileChecksum){
-				pos = serverFileChecksum.IndexOf(file);
-				
-				if (pos < 0)
-				{
-					// create a new file on the server
-					this.sendCommand(NEW_CMD, file.filePath);
-					this.sendFile(file.filePath);
-				}
-				else
-				{
-					// the file is also on the server
-					if (file.checksum != serverFileChecksum.ElementAt(pos).checksum)
-					{
-						// on the server there is a different version of the file
-						this.sendCommand(EDIT_CMD, file.filePath);
-						this.sendFile(file.filePath);
-					}
-					serverFileChecksum.RemoveAt(pos);
-				}
-			}
-			foreach (FileChecksum serverFile in serverFileChecksum)
-			{
-				// delete extra file on the server
-				this.sendCommand(DEL_CMD, serverFile.filePath);
-			}
-
-			// Do syncking
-			while (!thread_stopped)
-			{
-				Thread.Sleep(2000);
-			}
-		}
-
-
-		private void sendCommand(int command, string param1 = "", string param2 = "")
-		{
-			String message;
-			Byte[] data;
-			switch (command)
-			{
-				case START_CMD:
-					message = "START:" + param1;
-					break;
-				case EDIT_CMD:
-					message = "EDIT:" + param1;
-					break;
-				case DEL_CMD:
-					message = "DEL:" + param1;
-					break;
-				case NEW_CMD:
-					message = "NEW:" + param1;
-					break;
-				case RESTORE_CMD:
-					message = "RESTORE:" + param1;
-					break;
-				case GET_CMD:
-					message = "GET:" + param1 + ";" + param2;
-					break;
-				case ENDSYNC_CMD:
-					message = "ENDSYNC:";
-					break;
-				default:
-					message = "";
-					break;
-			}
-
-			data = System.Text.Encoding.ASCII.GetBytes(message);
-			networkStream.Write(data, 0, data.Length);
-		}
-
-		private void sendFile(String path)
-		{
-		}
-
-		private Boolean sendCheckList(List<FileChecksum>)
-		{
-			return true;
-		}
-
-		private String getFile()
-		{
-			return "";
-		}
-
-
         private String getClientDir()
         {
 
@@ -285,6 +176,8 @@ namespace sync_server
             string [] split = login.Split(new Char [] {: , ;} );
             return split;
         }
+        */
+        //Funzioni per gestire la versione
 
         private void resetVersion()
         {
@@ -306,6 +199,54 @@ namespace sync_server
         {
             return this.version;
         }
+
+        //Genera il file checksum delle cartelle e ricorsivamente dei file interni
+
+        private void generateServerChecksum(String dir)
+        {
+            String pattern= "["+ "_" + version.ToString() + "\\" + "Z" + "]" ;
+            string[] fileList = Directory.GetFiles(dir);
+            foreach (string filePath in fileList)
+            {
+                if(Regex.IsMatch(filePath, pattern))
+                {
+                    serverFileChecksum.Add(new FileChecksum(filePath));
+                }
+            }
+
+            // Recurse into subdirectories of this directory.
+            string[] subdirectoryList = Directory.GetDirectories(dir);
+            foreach (string subdirectoryPath in subdirectoryList)
+            {
+                this.generateServerChecksum(subdirectoryPath);
+            }
+        }
+
+
+
+        public String createCommand(int CMD, String path, String checksum){
+        
+            JsonObject command;
+
+        command  = new JsonObject(CMD.ToString);
+        command["Command"] = JsonValue.CreateStringValue();
+        command["Path"] = JsonValue.CreateStringValue(path);
+        command["Checksum"] = JsonValue.CreateStringValue(checksum);
+
+            return command.Stringify();
+        }
+
+        public String[] getCommand(String jsonoCMD)
+        {
+            String[] command = new String[3];
+            JsonValue jsonOBJ = JsonValue.Parse(jsonoCMD);
+            command[0] = jsonOBJ.GetObject().GetNamedString("Command");
+            command[1] = jsonOBJ.GetObject().GetNamedString("Path");
+            command[2] = jsonOBJ.GetObject().GetNamedString("Checksum");
+
+            return command;
+        }
+        
 
 
 	}
