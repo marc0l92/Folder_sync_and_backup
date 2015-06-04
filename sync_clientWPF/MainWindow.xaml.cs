11449f23ed5f48efaadf656aa4abcf08aed73a60
marc0l92 <lucarellamarco@libero.it>
2015-06-04 16:17:16 +0200

    provo una nuova grafica

---
+++ sync_clientWPF/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace sync_clientWPF
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void StartSync_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void StopSync_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Browse_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Restore_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+	}
+}
