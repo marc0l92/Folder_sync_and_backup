d1a43c85318793fdbf3f7bfa4feaf1d8a881249a
DrewNF <hause.blackwarhol@gmail.com>
2015-06-04 20:57:06 +0200

    Mod
    Async

---
+++ sync_server/syncClient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sync_server
+{
+    class syncClient
+    {
+        private String usrDir { set; get; }
+        private String usrNam { set; get; }
+        private String usrPwd { set; get; }
+        private List<FileChecksum> usrFileChecksum { set; get; }
+        private int vers { set; get; }
+        private Boolean syncEnd { set; get; } 
+
+    }
+}
