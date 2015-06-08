1b7861f595da25c95179af5da40e2380a6d309dc
marc0l92 <lucarellamarco@libero.it>
2015-06-08 22:45:29 +0200

    aggiornamento sql e creazione unit test

---
+++ SyncServerUnitTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SyncServerUnitTest
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void SqlTest()
+		{
+			sync_server.SyncSQLite syncSql = new sync_server.SyncSQLite();
+			
+		}
+	}
+}
